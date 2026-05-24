1c7b84623e4e2da9d036a16981bc48a37b5ef5c6
Abhishek-Chugh-123 <156787429+Abhishek-Chugh-123@users.noreply.github.com>
2026-05-24 17:39:12 +0530

    Update CorsConfig.java

--- src/main/java/com/codingshuttle/projects/lovable_clone/config/CorsConfig.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174", "http://localhost:5174")
+                        .allowedOrigins("http://localhost:5174", "http://localhost:5174","https://lovable-frontend-iota.vercel.app")
                         // Allow React app to access
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
